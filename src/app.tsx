80a6b8317b78a95c4a0026f0fb292a5dd9341d46
Sachin Jadhav <sachinjadhav@Sachins-MacBook-Pro.local>
2023-03-30 16:54:44 +0530

    add socket io

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
-
+import * as io from "socket.io-client";
 import Cube from "./components/cube";
 import Cylinder from "./components/cylinder";
 import Sphere from "./components/sphere";
@@ -12,26 +12,41 @@ import {
   DEFAULT_GEOMETRY_SIZE,
   GEOMETRY_MINIMUM_SIZE,
   GEOMETRY_MAXIMUM_SIZE,
+  GEOMETRY_UPDATE_INTERVAL_TIME,
 } from "./constants";
+
+const randomNumberGenerator = () => {
+  return (
+    Math.random() * (GEOMETRY_MAXIMUM_SIZE - GEOMETRY_MINIMUM_SIZE) +
+    GEOMETRY_MINIMUM_SIZE
+  ).toFixed(3);
+};
 
 function App() {
   const [, setForceUpdate] = useState(Date.now());
   const sizeRef = useRef(DEFAULT_GEOMETRY_SIZE);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      const updatedValue = (
-        Math.random() * (GEOMETRY_MAXIMUM_SIZE - GEOMETRY_MINIMUM_SIZE) +
-        GEOMETRY_MINIMUM_SIZE
-      ).toFixed(3);
+  const socket = io.connect("https://project-chat-application.herokuapp.com/");
 
-      console.log(updatedValue);
+  console.log("socket", socket);
+
+  useEffect(() => {
+    socket.emit("chat message", "emitting");
+
+    socket.on("chat message", () => {
+      console.log("rec chat message");
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    const geometryUpdateIntervalId = setInterval(() => {
+      const updatedValue = randomNumberGenerator(); // random number generator between provided MAX - MIN number
 
       sizeRef.current = parseFloat(updatedValue);
       setForceUpdate(Date.now());
-    }, 100);
+    }, GEOMETRY_UPDATE_INTERVAL_TIME);
 
-    return () => clearInterval(intervalId); //This is important
+    return () => clearInterval(geometryUpdateIntervalId); //This is important
   }, []);
 
   return (
--- src/constants.tsx
+++ src/constants.tsx
@@ -2,3 +2,4 @@ export const COLOR = "#00C0A3";
 export const DEFAULT_GEOMETRY_SIZE = 1;
 export const GEOMETRY_MINIMUM_SIZE = 0.75;
 export const GEOMETRY_MAXIMUM_SIZE = 1.25;
+export const GEOMETRY_UPDATE_INTERVAL_TIME = 100;
